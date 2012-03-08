2d8bf538d0478e025ed90ca3914c05cbc5c82d76
Hadi Michael <hadisalem7@gmail.com>
2012-03-08 13:10:27 +1100

    make geolocation more generic

--- PT Tracker/www/scripts/geolocation.js
+++ PT Tracker/www/scripts/geolocation.js
@@ -1,10 +1,12 @@
 function get_geoLocation() {
-	navigator.geolocation.getCurrentPosition(GL_onSuccess, GL_onError);
+	navigator.geolocation.getCurrentPosition(geoLocation_onSuccess, geoLocation_onError);
 }
 
-var GL_t;
+var geoLocation_timer;
 
-var GL_onSuccess = function(position) {
+var geoLocation_onSuccess = function(position) {
+	var speedINkph = position.coords.speed * (60*60/1000);
+	
 	var location = 
 		'Latitude: '          + position.coords.latitude          + '<br />' +
 		'Longitude: '         + position.coords.longitude         + '<br />' +
@@ -12,20 +14,20 @@ var GL_onSuccess = function(position) {
 		'Accuracy: '          + position.coords.accuracy          + '<br />' +
 		'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '<br />' +
 		'Heading: '           + position.coords.heading           + '<br />' +
-		'Speed: '             + position.coords.speed             + '<br />' +
+		'Speed: '             + position.coords.speed + ' | ' + speedINkph + '<br />' +
 		'Timestamp: '         + new Date(position.timestamp)      + '<br />';
 	
 	document.getElementById('geolocation').innerHTML = location;
 	
-	GL_t = setTimeout("get_geoLocation()",500);
+	geoLocation_timer = setTimeout("get_geoLocation()",500);
 }
 
 // onError Callback receives a PositionError object
-function GL_onError(error) {
+function geoLocation_onError(error) {
 	alert('code: '    + error.code    + '\n' +
 		  'message: ' + error.message + '\n');
 }
 
-function GL_stop() {
-	clearTimeout(GL_t);
+function geoLocation_stop() {
+	clearTimeout(geoLocation_timer);
 }
