37a488192aef3aecacf3ad15dc084d759b0f587f
Hadi Michael <hadisalem7@gmail.com>
2012-03-08 13:09:40 +1100

    make accelerometer more generic

--- PT Tracker/www/scripts/accelerometer.js
+++ PT Tracker/www/scripts/accelerometer.js
@@ -1,37 +1,33 @@
 // The watch id references the current `watchAcceleration`
-var watchID = null;
+var accelerometer_watchID = null;
 
 // Start watching the acceleration
-//
-function startWatch() {
+function accelerometer_startWatching() {	
 	
-	// Update acceleration every 500 milliseconds
-	var options = { frequency: 500 };
+	var options = { frequency: 500 }; //Update acceleration every 500 milliseconds
 	
-	watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+	accelerometer_watchID = navigator.accelerometer.watchAcceleration(accelerometer_onSuccess, accelerometer_onError, options);
 }
 
 // Stop watching the acceleration
-//
-function stopWatch() {
+function accelerometer_stopWatching() {
 	if (watchID) {
-		navigator.accelerometer.clearWatch(watchID);
-		watchID = null;
+		navigator.accelerometer.clearWatch(accelerometer_watchID);
+		accelerometer_watchID = null;
 	}
 }
 
 // onSuccess: Get a snapshot of the current acceleration
-//
-function onSuccess(acceleration) {
+function accelerometer_onSuccess(acceleration) {
 	var element = document.getElementById('acceleration');
+	
 	element.innerHTML = 'Acceleration X: ' + acceleration.x + '<br />' +
 						'Acceleration Y: ' + acceleration.y + '<br />' +
 						'Acceleration Z: ' + acceleration.z + '<br />' +
-						'Timestamp: '      + acceleration.timestamp + '<br />';
+						'Timestamp: '      + new Date(acceleration.timestamp) + '<br />';
 }
 
 // onError: Failed to get the acceleration
-//
-function onError() {
-	alert('onError!');
+function accelerometer_onError() {
+	alert('Error: Could not watch accelerometer!');
 }
