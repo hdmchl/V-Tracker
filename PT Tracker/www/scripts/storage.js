200702055989e057330cd6efe690776a8ea2a41e
Hadi Michael <hadisalem7@gmail.com>
2012-03-15 11:16:16 +1100

    add geolocation information to a database

--- PT Tracker/www/scripts/storage.js
+++ PT Tracker/www/scripts/storage.js
@@ -1,33 +1,59 @@
 function storage_init() {
 	var db = window.openDatabase("PTTracker_db", "1.00", "PT Tracker DB", 2 * 1024*1024); //create a 2MBs database
+	console.log("Database opened!");
 	
-	db.transaction(populateDB, errorCB, successCB);
+	db.transaction(clear, storage_errorCB, storage_successCB);
+}
+
+function clear(tx) {
+	tx.executeSql('DROP TABLE IF EXISTS DEMO;');
+	tx.executeSql('DROP TABLE IF EXISTS GEOLOCATION;');
 	
-	db.transaction(queryDB, errorCB);
+	tx.executeSql('CREATE TABLE IF NOT EXISTS GEOLOCATION (id unique, Latitude, Longitude, Altitude, Accuracy, AltitudeAccuracy, Heading, Speed, Timestamp);');
 }
 
-function populateDB(tx) {
-	tx.executeSql('DROP TABLE IF EXISTS DEMO');
-	tx.executeSql('CREATE TABLE IF NOT EXISTS DEMO (id unique, data)');
-	tx.executeSql('INSERT INTO DEMO (id, data) VALUES (1, "First row")');
-	tx.executeSql('INSERT INTO DEMO (id, data) VALUES (2, "Second row")');
+var geoLocation_position = '';
+function updateGeoLocationTable(position) {
+	
+	geoLocation_position =
+					'"' + position.coords.latitude          + '",' +
+					'"' + position.coords.longitude         + '",' +
+					'"' + position.coords.altitude          + '",' +
+					'"' + position.coords.accuracy          + '",' +
+					'"' + position.coords.altitudeAccuracy  + '",' +
+					'"' + position.coords.heading           + '",' +
+					'"' + position.coords.speed             + '",' +
+					'"' + new Date(position.timestamp)      + '"';
+
+	var db = window.openDatabase("PTTracker_db", "1.00", "PT Tracker DB", 2 * 1024*1024); //create a 2MBs database
+	db.transaction(populateGeoLocationTable, storage_errorCB, storage_successCB);
 }
 
-function errorCB(err) {
-    alert("Error processing SQL: " + err.code + ", " + err.message);
+var rowIdCounter = 0;
+function populateGeoLocationTable(tx) {
+	//console.log(geoLocation_position);
+	
+	tx.executeSql('INSERT INTO GEOLOCATION VALUES (' + rowIdCounter + ',' + geoLocation_position + ');');
+	rowIdCounter++;
 }
 
-function successCB() {
-    alert("success!");
+function storage_errorCB(error) {
+    console.log("Error processing SQL: " + error.code + ", " + error.message);
 }
 
+function storage_successCB() {
+    //console.log("SQL processed successfully!");
+}
+
+
+//SHOW DATA
 function storage_show() {
 	var db = window.openDatabase("PTTracker_db", "1.00", "PT Tracker DB", 2 * 1024*1024); //create a 2MBs database
-	db.transaction(queryDB, errorCB);
+	db.transaction(queryDB, storage_errorCB);
 }
 
 function queryDB(tx) {
-    tx.executeSql('SELECT * FROM DEMO', [], querySuccess, errorCB);
+    tx.executeSql('SELECT * FROM GEOLOCATION', [], querySuccess, storage_errorCB);
 }
 
 function querySuccess(tx, results) {
@@ -35,13 +61,13 @@ function querySuccess(tx, results) {
 	
 	var len = results.rows.length;
 	
-	var table = '<p>DEMO table: ' + len + ' rows found.</p><p>';
+	var table = '<p>GEOLOCATION table: ' + len + ' rows found.</p>';
 	
 	for (var i=0; i<len; i++){
-		table = table + '<br>' + "Row = " + i + " ID = " + results.rows.item(i).id + " Data =  " + results.rows.item(i).data;
+		table = table + '<p>' +     "Row Id = "    + results.rows.item(i).id + '<br />' +
+									"Latitude = "  + results.rows.item(i).Latitude + '<br />' +
+								    "Longitude = " + results.rows.item(i).Longitude + '</p>';
 	}
-	
-	table = table + '</p>';
 	 
 	document.getElementById('databases').innerHTML = table;
 }
