d372dc7f8ce422fa04201d1c0687a4ea0469c718
Hadi Michael <hadisalem7@gmail.com>
2012-03-16 19:18:45 +1100

    initialise file

--- PT Tracker/www/scripts/localstorage.js
+++ PT Tracker/www/scripts/localstorage.js
@@ -1,12 +1,1 @@
-function saveGameState() {
-    if (!supportsLocalStorage()) { return false; }
-    localStorage["halma.game.in.progress"] = gGameInProgress;
-    for (var i = 0; i < kNumPieces; i++) {
-		localStorage["halma.piece." + i + ".row"] = gPieces[i].row;
-		localStorage["halma.piece." + i + ".column"] = gPieces[i].column;
-    }
-    localStorage["halma.selectedpiece"] = gSelectedPieceIndex;
-    localStorage["halma.selectedpiecehasmoved"] = gSelectedPieceHasMoved;
-    localStorage["halma.movecount"] = gMoveCount;
-    return true;
-}
+//code will go here as necessary... at the moment, I am using SQLite3
