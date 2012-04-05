975d0c92932c67dfecc543ddb2708c7ad47a359b
Hadi Michael <hadisalem7@gmail.com>
2012-04-05 14:30:43 +1000

    commit stable build

--- PT Tracker/www/scripts/accelerometer.js
+++ PT Tracker/www/scripts/accelerometer.js
@@ -22,6 +22,21 @@ var gravity = -1.025;
 function accelerometer_onSuccess(acceleration) {
 	updateAccelerometerTable(acceleration); //update SQL
 
+	var x = parseFloat(acceleration.x);
+	var y = parseFloat(acceleration.y);
+	var z = parseFloat(acceleration.z);
+	
+	var r1 = Math.atan2(z,x);
+	var r2 = Math.asin( Math.max(gravity / Math.sqrt(Math.pow(x,2)+Math.pow(z,2)), -1) );
+	var roll = (r1 - r2) * 180/Math.PI;
+	
+	var p1 = Math.asin( Math.max(gravity / Math.sqrt(Math.pow(y,2)+Math.pow(z,2)), -1) );
+	var p2 = Math.atan2(z,y); 
+	var pitch = (p1 - p2) * 180/Math.PI;
+	
+	console.log('Roll: ' + roll);
+	console.log('Pich: ' + pitch);
+	
 	//display results in real-time
 	var element = document.getElementById('acceleration');
 	
--- PT Tracker/www/scripts/scripts.js
+++ PT Tracker/www/scripts/scripts.js
@@ -4,48 +4,51 @@ function clearDiv(divId) {
 	document.getElementById(divId).innerHTML = '';
 }
 
-var buffer1 = '';
-var buffer2 = '';
-var buffer3 = '';
-var buffer4 = '';
-var buffer5 = '';
+var alertsConsoleLog = [];
 var alertsConsoleCounter = 1;
 function consoleLog(message) {
 	console.log(message);
 	
-	buffer1 = buffer2;
-	buffer2 = buffer3;
-	buffer3 = buffer4;
-	buffer4 = buffer5;
-	buffer5 = alertsConsoleCounter + ") " + message + "<br />";
-
-	document.getElementById('alertsConsole').innerHTML = buffer1 + buffer2 + buffer3 + buffer4 + buffer5;
-		
+	alertsConsoleLog[alertsConsoleCounter] = alertsConsoleCounter + ") " + message + "<br />";
+	
+	var buffer = alertsConsoleLog[alertsConsoleCounter-5] + alertsConsoleLog[alertsConsoleCounter-4] + alertsConsoleLog[alertsConsoleCounter-3] + alertsConsoleLog[alertsConsoleCounter-2] + alertsConsoleLog[alertsConsoleCounter-1] + alertsConsoleLog[alertsConsoleCounter];
+	
+	document.getElementById('alertsConsole').innerHTML = buffer;
+	
 	alertsConsoleCounter++;
+}
+
+function deleteAlertsConsoleLog() {
+	alertsConsoleLog.length = 0;
+	alertsConsoleCounter = 1;
+	
+	document.getElementById('alertsConsoleLog').innerHTML = '';
+}
+
+function displayAlertsConsoleLog() {
+	var buffer = '';
+
+	for(i=1;i<alertsConsoleLog.length;i++) {
+		buffer = buffer + alertsConsoleLog[i];
+	}
+
+	document.getElementById('alertsConsoleLog').innerHTML = buffer;
 }
 
 function startDataCollection() {
 	document.getElementById('loader').style.visibility = 'visible';
 	
 	accelerometer_startWatching();
-	compass_startWatching()
-	
+	compass_startWatching();
+}
+
+function stopDataCollection() {
+	accelerometer_stopWatching();
+	compass_stopWatching();
+
+	document.getElementById('loader').style.visibility = 'hidden';
 }
 
 function analyse() {
-	var x = parseFloat(acceleration.x);
-	var y = parseFloat(acceleration.y);
-	var z = parseFloat(acceleration.z);
-	
-	var r1 = Math.atan2(z,x);
-	var r2 = Math.asin( Math.max(gravity / Math.sqrt(Math.pow(x,2)+Math.pow(z,2)), -1) );
-	var roll = (r1 - r2) * 180/Math.PI;
-	
-	var p1 = Math.asin( Math.max(gravity / Math.sqrt(Math.pow(y,2)+Math.pow(z,2)), -1) );
-	var p2 = Math.atan2(z,y); 
-	var pitch = (p1 - p2) * 180/Math.PI;
-	
-	console.log('Roll: ' + roll);
-	console.log('Pich: ' + pitch);
 	
 }
