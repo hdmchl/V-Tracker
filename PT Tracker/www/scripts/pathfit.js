3ec824561ea2193bbf788a4cb2b436b9550b6c1c
Hadi Michael <hadisalem7@gmail.com>
2012-05-10 17:26:17 +1000

    show graph once it's plotted

--- PT Tracker/www/scripts/pathfit.js
+++ PT Tracker/www/scripts/pathfit.js
@@ -9,13 +9,15 @@ function pathFit() {
 	
 	var y1 = [0, -7, -5, -2, -8, -40];
 	var x1 = [0, 25, 50, 75, 100, 125];
+	
 	var s1 = numeric.spline(x1,y1);
 	 
 	var x2 = numeric.linspace(0,125,125);
 	var y2 = s1.at(x2);
 	 
-	console.log(s1.at(80));
+	console.log(s1);
 	
 	$.plot($("#graphPlaceholder"), [ numeric.transpose([x2,y2]) ]);
-
+	
+	document.getElementById('graphPlaceholder').style.display = 'block';
 }
