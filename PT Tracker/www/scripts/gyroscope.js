225c09aefb1fec7352ec05bb81bfc3d4950647d9
Hadi Michael <hadisalem7@gmail.com>
2012-05-13 17:44:15 +1000

    sample gyro every sec.

--- PT Tracker/www/scripts/gyroscope.js
+++ PT Tracker/www/scripts/gyroscope.js
@@ -4,27 +4,40 @@
  * Faculty of Engineering, Monash University (Australia)
  *
  */
-
 var gyroscope = {
+	timer: null,
+	
+	orientation: {alpha: null, beta: null, gamma: null},
+	
 	startWatching:function() {
 		window.addEventListener("deviceorientation", gyroscope.updateOrientation);
-		
+		gyroscope.timer = setTimeout(gyroscope.sample(),1000);
 		consoleLog("gyroscope.watch started");
 	},
 	
 	stopWatching:function() {
 		window.removeEventListener("deviceorientation", gyroscope.updateOrientation);
+		clearTimeout(gyroscope.timer);
 		consoleLog("gyroscope.watch stopped");
 	},
+	
+	sample:function() {
+		updateTable.gyroscope(this.orientation); //update SQL
+		gyroscope.timer = setTimeout(function() {gyroscope.sample();}, 1000);
+	},
 
-	updateOrientation:function(gyroscopeOrientation) {
+	updateOrientation:function(orientation) {
+		gyroscope.orientation.alpha = orientation.alpha;
+		gyroscope.orientation.beta = orientation.beta;
+		gyroscope.orientation.gamma = orientation.gamma;
+		
 		//display results in real-time
 		if (showRealtimeData) {
 			var element = document.getElementById('gyroscope');
 			
-			element.innerHTML = 	'Alpha: '  + gyroscopeOrientation.alpha +
-									'<br>Beta: '  + gyroscopeOrientation.beta  +
-									'<br>Gamma: ' + gyroscopeOrientation.gamma;
+			element.innerHTML = 	'Alpha: '  + orientation.alpha +
+									'<br>Beta: '  + orientation.beta  +
+									'<br>Gamma: ' + orientation.gamma;
 		}
 	}
 }
