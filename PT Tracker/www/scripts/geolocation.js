d4362e295c96b98916a05963314173d7eda14ccc
Hadi Michael <hadisalem7@gmail.com>
2012-05-13 11:08:14 +1000

    watch geolocation, rather than "get" using a timer

--- PT Tracker/www/scripts/geolocation.js
+++ PT Tracker/www/scripts/geolocation.js
@@ -6,21 +6,36 @@
  */
 
 var geoLocation_timer = null;
+var geoLocation_watchID = null;
 
 function geoLocation_startWatching() {
-	geoLocation_get();
-	consoleLog("geoLocation watch started");
+	geoLocation_watchID = navigator.geolocation.watchPosition(geoLocation_onSuccess, geoLocation_onError, {
+																frequency: 10000, //frequency is not part of W3C and will not be supported in the future
+																maximumAge: 10000, //Accept a cached position whose age is no greater than the specified time
+																timeout: 5000, //max time between call, and receipt
+																enableHighAccuracy: true
+															  });	
+	consoleLog("geoLocation watch started, ID: " + geoLocation_watchID);
 }
 
 function geoLocation_get() {
 	navigator.geolocation.getCurrentPosition(geoLocation_onSuccess, geoLocation_onError);
 }
 
+var c = 0;
 var geoLocation_onSuccess = function(position) {	
 	updateGeoLocationTable(position); //update SQL
 	localisation_setUserLocation(position); //set localise data
 	
-	var speedINkph = position.coords.speed * (60*60/1000); //calculate speed in km/hr
+	
+	c = c+1;
+	console.log(c);
+	if (c==7){
+		console.log('yup');
+		notifications_showAlert()
+	}
+	
+	if (position.coords.speed >=0) {var speedINkph = position.coords.speed * (60*60/1000);} else {var speedINkph = -1;} //calculate speed in km/hr
 	
 	//display results in real-time
 	if (showRealtimeData) {
@@ -36,20 +51,15 @@ var geoLocation_onSuccess = function(position) {
 	
 	document.getElementById('geolocation').innerHTML = location;
 	}
-	
-	geoLocation_timer = setTimeout("geoLocation_get()", 400); //send off a new request after 400ms
 }
 
 // onError Callback receives a PositionError object
 function geoLocation_onError(error) {
 	consoleLog('Geolocation error code: ' + error.code + ' & Message: ' + error.message);
-	
-	//try again after 2 seconds	  
-	geoLocation_timer = setTimeout("geoLocation_get()", 2000);
 }
 
 function geoLocation_stopWatching() {
-	clearTimeout(geoLocation_timer);
+	navigator.geolocation.clearWatch(geoLocation_watchID);
 	geoLocation_timer = null;
-	consoleLog("geoLocation_timer stopped");
+	consoleLog("geoLocation watch stopped, ID: " + geoLocation_watchID);
 }
