80f8357ab4543461ec188604a85738cf49b8c62e
Hadi Michael <hadisalem7@gmail.com>
2012-05-16 22:42:10 +1000

    rename to Obj, use this.

--- PT Tracker/www/scripts/compass.js
+++ PT Tracker/www/scripts/compass.js
@@ -5,39 +5,51 @@
  *
  */
 
-var compass = {
+var compassObj = {
 	watchID: null,
-	options: { frequency: 1000 }, //Set frequency for compass in milliseconds
+	
+	options: { frequency: 1000 }, //Set update interval in milliseconds
+	
+	data: {magneticHeading: null, trueHeading: null, headingAccuracy: null, timestamp:null},
 
 	// Start watching the compass
 	startWatching:function() {	
-		compass.watchID = navigator.compass.watchHeading(compass.onSuccess, compass.onError, compass.options);
-		
-		consoleLog("compass.watch started, ID: " + compass.watchID);
+		this.watchID = navigator.compass.watchHeading(this.onSuccess, this.onError, this.options);
+		consoleLog("compass.watch started, ID: " + this.watchID);
 	},
 	
 	// Stop watching the compass
 	stopWatching:function() {
-		if (compass.watchID) {
-			navigator.compass.clearWatch(compass.watchID);
+		if (this.watchID) {
+			navigator.compass.clearWatch(this.watchID);
 			consoleLog("compass.watch stopped");	
 		}
 	},
 	
-	// onSuccess: display a snapshot of the current heading
+	// onSuccess: take a snapshot of the current heading - can't use "this." in here...
 	onSuccess:function(compassHeading) {
 		updateTable.compass(compassHeading); //update SQL
 		
-		//display results in real-time
-		if (showRealtimeData) {
-			var element = document.getElementById('compass');
-		
-			element.innerHTML = 'Heading: ' + compassHeading.magneticHeading;
-		}
+		compassObj.data.magneticHeading = compassHeading.magneticHeading;
+		compassObj.data.trueHeading = compassHeading.trueHeading;
+		compassObj.data.headingAccuracy = compassHeading.headingAccuracy;
+		compassObj.data.timestamp = compassHeading.timestamp;
+
+		if (showRealtimeData) {compassObj.updateDisplay();}
 	},
 	
 	// onError: Failed to get the compass heading
 	onError:function(compassError) {
 		consoleLog('Could not get compass data! Error: ' + compassError.code);
+	},
+
+	//display results in real-time
+	updateDisplay:function() {
+		var element = document.getElementById('compass');
+		
+			element.innerHTML = 'Magnetic heading: ' + Math.round(100000*parseFloat(this.data.magneticHeading))/100000 + '<br />' +
+								'True heading: ' + Math.round(100000*parseFloat(this.data.trueHeading))/100000 + '<br />' +
+								'Heading accuracy: ' + this.data.headingAccuracy + '<br />' +
+								'Timestamp: ' + new Date(this.data.timestamp);
 	}
 }
