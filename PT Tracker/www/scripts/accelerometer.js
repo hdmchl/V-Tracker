45ebd79991651fbc26c6591d3953490d717a8968
Hadi Michael <hadisalem7@gmail.com>
2012-05-16 22:41:53 +1000

    use 'this.' where possible

--- PT Tracker/www/scripts/accelerometer.js
+++ PT Tracker/www/scripts/accelerometer.js
@@ -5,20 +5,62 @@
  *
  */
 
-var accelerometer = {
+var accelerometerObj = {
 	watchID: null,
-	options: { frequency: 200 }, //Set frequency for acceleration in milliseconds
 	
-	accelX: 0,
-	accelY: 0,
-	accelZ: 9,
+	options: { frequency: 200 }, //Set update interval in milliseconds - check wiki if you're unsure of values
 	
-	calculateRPY:function(acceleration,orientation) {
+	data: {x: 0, y: 0, z: 0, timestamp: null},
+	
+	// Start watching the acceleration
+	startWatching:function() {
+		this.watchID = navigator.accelerometer.watchAcceleration(this.onSuccess, this.onError, this.options);
+		consoleLog("accelerometer.watch started, ID: " + this.watchID);
+	},
+	
+	// Stop watching the acceleration
+	stopWatching:function() {
+		if (this.watchID) {
+			navigator.accelerometer.clearWatch(this.watchID);
+			consoleLog("accelerometer.watch stopped");	
+		}
+	},
+	
+	// onSuccess: take a snapshot of the current acceleration - can't use "this." in here...
+	onSuccess:function(accelerationData) {
+		updateTable.accelerometer(accelerationData); //update SQL
+		
+		accelerometerObj.data.x = accelerationData.x;
+		accelerometerObj.data.y = accelerationData.y;
+		accelerometerObj.data.z = accelerationData.z;
+		accelerometerObj.data.timestamp = accelerationData.timestamp;
+		
+		if (showRealtimeData) {accelerometerObj.updateDisplay('tet');}
+	},
+	
+	// onError: Failed to get the acceleration
+	onError:function() {
+		consoleLog('Error: Could not get accelerometer data!');
+	},
+	
+	//display results in real-time
+	updateDisplay:function() {
+		var element = document.getElementById('acceleration');
+		
+		element.innerHTML = 'Acceleration X: ' + Math.round(100000*parseFloat(this.data.x))/100000 + '<br />' +
+							'Acceleration Y: ' + Math.round(100000*parseFloat(this.data.y))/100000 + '<br />' +
+							'Acceleration Z: ' + Math.round(100000*parseFloat(this.data.z))/100000 + '<br />' +
+							'Timestamp: '      + new Date(this.data.timestamp) + '<br />' +
+															
+							this.calculateRP();
+	},
+	
+	calculateRP:function() {
 		var gravity = -10.05;
 		
-		var x = -parseFloat(acceleration.x);
-		var y = -parseFloat(acceleration.y);
-		var z = -parseFloat(acceleration.z);
+		var x = -parseFloat(this.data.x);
+		var y = -parseFloat(this.data.y);
+		var z = -parseFloat(this.data.z);
 		
 		var r1 = Math.atan2(z,x);
 		var r2 = Math.asin( Math.max(gravity / Math.sqrt(Math.pow(x,2)+Math.pow(z,2)), -1) );
@@ -28,14 +70,13 @@ var accelerometer = {
 		var p2 = Math.atan2(z,y); 
 		var pitch = (p1 - p2) * 180/Math.PI;
 		
-		if (orientation == 'roll') {
-			return Math.round(100*roll)/100;
-		} else if (orientation == 'pitch') {
-			return Math.round(100*pitch)/100 ;
-		}
+		var ret =   'Roll: '  + Math.round(100*roll)/100 + '<br />' +
+					'Pitch: ' + Math.round(100*pitch)/100 + '<br />';
+		
+		return ret;
 	},
 
-	highPassFilter:function(acceleration) {
+	/*highPassFilter:function() {
 		var kFilteringFactor = 0.96;
 		
 		// Subtract the low-pass value from the current value to get a simplified high-pass filter
@@ -43,46 +84,8 @@ var accelerometer = {
 	    this.accelY = acceleration.y - ( (acceleration.y * kFilteringFactor) + (this.accelY * (1.0 - kFilteringFactor)) );
 	    this.accelZ = acceleration.z - ( (acceleration.z * kFilteringFactor) + (this.accelZ * (1.0 - kFilteringFactor)) );
 		
-		var filtered = this.accelX + ' || ' + this.accelY + ' || ' + this.accelZ; 
+		var filtered = 'Filtered: ' + this.accelX + ' || ' + this.accelY + ' || ' + this.accelZ; 
 		
 		return filtered;
-	},
-	
-	// Start watching the acceleration
-	startWatching:function() {	
-		accelerometer.watchID = navigator.accelerometer.watchAcceleration(accelerometer.onSuccess, accelerometer.onError, accelerometer.options);
-		
-		consoleLog("accelerometer.watch started, ID: " + accelerometer.watchID);
-	},
-	
-	// Stop watching the acceleration
-	stopWatching:function() {
-		if (accelerometer.watchID) {
-			navigator.accelerometer.clearWatch(accelerometer.watchID);
-			consoleLog("accelerometer.watch stopped");	
-		}
-	},
-	
-	// onSuccess: Get a snapshot of the current acceleration
-	onSuccess:function(acceleration) {
-		updateTable.accelerometer(acceleration); //update SQL
-			
-		//display results in real-time
-		if (showRealtimeData) {
-			var element = document.getElementById('acceleration');
-			
-			element.innerHTML = 'Acceleration X: ' + Math.round(10000*parseFloat(acceleration.x))/10000 + '<br />' +
-								'Acceleration Y: ' + Math.round(10000*parseFloat(acceleration.y))/10000 + '<br />' +
-								'Acceleration Z: ' + Math.round(10000*parseFloat(acceleration.z))/10000 + '<br />' +
-								'Timestamp: '      + new Date(acceleration.timestamp) + '<br />' +
-								'Roll: '           + accelerometer.calculateRPY(acceleration,'roll') + '<br />' +
-								'Pitch: '          + accelerometer.calculateRPY(acceleration,'pitch') + '<br />' +
-			                    'filtered: '       + accelerometer.highPassFilter(acceleration) + '<br />';
-		}
-	},
-	
-	// onError: Failed to get the acceleration
-	onError:function() {
-		consoleLog('Error: Could not get accelerometer data!');
-	}
+	}*/
 }
