ba42fc67c339b58bc5ccc9216443ed595a7ac506
Hadi Michael <hadisalem7@gmail.com>
2012-07-16 19:51:37 +1000

    set default transition modes

--- V-Tracker/www/scripts/jquery-mods.js
+++ V-Tracker/www/scripts/jquery-mods.js
@@ -1,1 +1,6 @@
 //Modifications to jQuery are done in here...
+
+$(document).bind("mobileinit", function(){
+	$.mobile.defaultPageTransition = 'none';
+	$.mobile.defaultDialogTransition = 'none';			 
+});
