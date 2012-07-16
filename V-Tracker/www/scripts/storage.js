81d2d2c8e7832edeff932e0cffe70a161f236ad9
Hadi Michael <hadisalem7@gmail.com>
2012-07-17 03:39:23 +1000

    clean up storage API

--- V-Tracker/www/scripts/storage.js
+++ V-Tracker/www/scripts/storage.js
@@ -13,36 +13,33 @@ var storage = {
 	//********************************** INITIALISE STORAGE ***********************************//
 	init:function() {
 		db = window.openDatabase("VTracker_db", "1.00", "V-Tracker DB", 2 * 1024*1024); //open a 2MBs database
-
-		storage.createTable(consoleLogObj,'CONSOLELOG');
-		
 		storage.ready = true;
 		console.log("Database opened. Storage initiated. Storage ready: " + storage.ready);
 	},
 	//******************************** END INITIALISE STORAGE *********************************//
 	
-	//************************************ RESET DATABASES ************************************//
+	//************************************** RESET TABLES *************************************//
 	reset:function() {
-		storage.ready = false;
 		storage.dropTable(storage.dbTables); //drop all tables we know about at this stage
-		console.log("Database tables reset! Storage ready: " + storage.ready);
+		console.log("Database tables reset.");
 	},
-	//********************************** END RESET DATABASES **********************************//
+	//************************************ END RESET TABLES ***********************************//
 	
 	//************************************* CREATE TABLE **************************************//
-	createTable:function(parentObject, tableName) {
-		//Specify schema for table based on the object's 'data' properties
-		var dbFields = "id integer primary key autoincrement";
-		for (var i in parentObject["data"]) {
-			dbFields = dbFields + "," + i;
+	createTable:function(tableFieldsAry, tableName) {
+		//Specify schema for table based on the fields array
+		var tableFields = "id integer primary key autoincrement";
+		
+		for (var i in tableFieldsAry) {
+			tableFields = tableFields + "," + i;
 		}
 		
 		//create table
 		db.transaction(function (tx) {
-					   tx.executeSql('CREATE TABLE IF NOT EXISTS ' + tableName + ' (' + dbFields + ');');
+					   tx.executeSql('CREATE TABLE IF NOT EXISTS ' + tableName + ' (' + tableFields + ');');
 					   }, storage.errorCB, storage.successCB);
 		
-		storage.getDBTables();   		   
+		storage.getDBTables();
 	},
 	
 	//*********************************** END CREATE TABLE ************************************//
@@ -50,107 +47,50 @@ var storage = {
 	//************************************** DROP TABLE ***************************************//
 	dropTable:function(tableNames) {
 		db.transaction(function (tx) { for (var i=0;i<tableNames.length;i++) {
-											if(tableNames[i] == "sqlite_sequence") {continue;}
-											tx.executeSql('DROP TABLE IF EXISTS ' + tableNames[i] + ';');
-											}
-									  }, storage.errorCB, storage.init);
+						   if(tableNames[i] == "sqlite_sequence") {continue;}
+						   tx.executeSql('DROP TABLE IF EXISTS ' + tableNames[i] + ';');
+						   }
+						   }, storage.errorCB, function(){storage.ready = true;});
+
 		storage.getDBTables();
 	},
 	//************************************ END DROP TABLE *************************************//
 	
-	//************************************ ERROR HANDLING *************************************//
-	errorCB:function(error) {
-	    consoleLog.add("Error processing SQL: " + error.code + " | " + error.message);
-	},
-	
-	successCB:function(toStore) {
-		//=== do not call the consoleLog object in here, because you'll get an infinite loop on success ===//
-	    
-	    //console.log("SQL processed successfully!");
-	},
-	//********************************** END ERROR HANDLING ***********************************//
-	
 	//******************************** INSERT DATA INTO TABLES ********************************//
-	insertInTable:function(tableName, dataType, data) {
-		switch(dataType) {
-			//console data
-			case "consoleData":
-				var message = data;
-				var toStore = '"' + new Date(new Date().getTime()) + '",' +
-				              '"' + message + '"';
-				
-				db.transaction(function (tx) {tx.executeSql('INSERT INTO CONSOLELOG VALUES (null,' + toStore + ');');
-											 }, function (error) { //onError
-										   			console.log("Error logging: " + message + " | Details: " + error.code + ", " + error.message);
-							   				  					}, storage.successCB);
-				return;
-			
-			//accelerometer data
-			case "accelerationData":
-				var accelerationData = data;
-				var toStore =	'"' + new Date(accelerationData.timestamp) + '",' +
-								'"' + accelerationData.x          + '",' +
-								'"' + accelerationData.y          + '",' +
-								'"' + accelerationData.z          + '"';
-				break;
-
-			//compass data
-			case "compassData":
-				var compassHeading = data;
-				var toStore = 	'"' + new Date(compassHeading.timestamp)  + '",' +               
-								'"' + compassHeading.magneticHeading      + '",' + 
-								'"' + compassHeading.trueHeading    	  + '",' + 
-								'"' + compassHeading.headingAccuracy      + '"';
-				break;
-
-			//gyroscope data
-			case "gyroData":
-				var gyroData = data;
-				var toStore = 	'"' + new Date(gyroData.timestamp) 	+ '",' +               
-								'"' + gyroData.alpha    	+ '",' +
-								'"' + gyroData.beta     	+ '",' +
-								'"' + gyroData.gamma    	+ '"';
-				break;
-							
-			//geolocation data
-			case "geolocationData":
-				var position = data;
-				var toStore =	'"' + new Date(position.timestamp)      + '",' +
-								'"' + position.coords.latitude          + '",' +
-								'"' + position.coords.longitude         + '",' +
-								'"' + position.coords.altitude          + '",' +
-								'"' + position.coords.accuracy          + '",' +
-								'"' + position.coords.altitudeAccuracy  + '",' +
-								'"' + position.coords.heading           + '",' +
-								'"' + position.coords.speed             + '"';
-				break;
-			
-			default: console.log("Error: Could not format data for storage!");
-		}
-							
-		db.transaction(function (tx){
-	  					 tx.executeSql('SELECT name, sql FROM sqlite_master WHERE type="table" AND name = "' + tableName + '";', [], function (tx, results) {
-							  var columnParts = results.rows.item(0).sql.replace(/^[^\(]+\(([^\)]+)\)/g, '$1').split(',');
-							  //console.log(columnParts);
-							  
-							  //----- INSERT data into table, if we have the correct number of measurements
-							  if (data.length == columnParts.length-1) {
-							  	db.transaction(function (tx) {
-										tx.executeSql('INSERT INTO ' + tableName + ' VALUES (null,' + toStore + ');');
-								}, storage.errorCB, storage.successCB);
-							  }
-							  //----- END
-							  
-						}, storage.errorCB);
-		}, storage.errorCB, storage.successCB);
+	insertIntoTable:function(tableName, data) {	
+		db.transaction(function (tx) {tx.executeSql('INSERT INTO ' + tableName + ' VALUES (null,' + data + ');');
+									 }, storage.errorCB, storage.successCB);				
 	},
 	//****************************** END INSERT DATA INTO TABLES ******************************//
 	
-	//************************ RETRIEVING TABLES AND LENGTHS FROM DB **************************//	
-	dbTables: [], //declare array of database tables
+	//************************************ ERROR HANDLING *************************************//
+	errorCB:function(error) {
+	    console.log("Error processing SQL: " + error.code + " | " + error.message);
+	},
 	
+	successCB:function(toStore) {
+		//console.log("SQL processed successfully!");
+	},
+	//********************************** END ERROR HANDLING ***********************************//
+	
+	//*********************************** GET TABLE COLUMNS ***********************************//
+	getTableColumns:function(tableName) {
+		var columnParts = null;
+		db.transaction(function (tx){
+	  					 tx.executeSql('SELECT name, sql FROM sqlite_master WHERE type="table" AND name = "' + tableName + '";', [], function (tx, results) {
+							  columnParts = results.rows.item(0).sql.replace(/^[^\(]+\(([^\)]+)\)/g, '$1').split(',');
+							  console.log(columnParts);
+							  //if necessary, do more stuff here...
+						}, storage.errorCB);
+		}, storage.errorCB, storage.successCB);
+	},
+	//********************************* END GET TABLE COLUMNS *********************************//
+	
+	//******************************** GET LIST OF TABLES IN DB *******************************//	
+	dbTables: [], //declare array of database tables
+
 	getDBTables:function() {
-		storage.dbTables = []; //clear arry
+		storage.dbTables = []; //clear array
 
 		// get a list of all the available tables and load into the dbTables array - when completed, getTableLengths
 		db.transaction(function (tx) {
@@ -159,9 +99,11 @@ var storage = {
 							   				 	for (var i=1; i<results.rows.length; i++) {storage.dbTables[i-1] = results.rows.item(i).name;}
 							   				 }
 									 }, storage.errorCB);
-					   }, storage.errorCB, function() {console.log("dbTables updated: " + storage.dbTables)});
+					   }, storage.errorCB, function() {console.log("dbTables was updated: " + storage.dbTables)});
 	},
+	//****************************** END GET LIST OF TABLES IN DB *****************************//
 	
+	//******************************* GET LENGTH OF TABLES IN DB ******************************//
 	queryCounter: 0,
 	getDBTableLengths:function() {
 		//if this is the first run, then set the output window
@@ -194,5 +136,5 @@ var storage = {
 									 }, storage.errorCB);
 						   }, storage.errorCB, storage.getDBTableLengths);	
 	}
-	//********************** END RETRIEVING TABLES AND LENGTHS FROM DB ************************//		
+	//***************************** END GET LENGTH OF TABLES IN DB ****************************//		
 }
