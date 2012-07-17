eb37adf62117de791b8594a8f377d331dcd9c775
Hadi Michael <hadisalem7@gmail.com>
2012-07-17 14:33:39 +1000

    add comment

--- V-Tracker/www/scripts/jquery-mods.js
+++ V-Tracker/www/scripts/jquery-mods.js
@@ -1,6 +1,7 @@
 //Modifications to jQuery are done in here...
 
 $(document).bind("mobileinit", function(){
+	//set default transitions to "none", it looks cleaner
 	$.mobile.defaultPageTransition = 'none';
 	$.mobile.defaultDialogTransition = 'none';			 
 });
