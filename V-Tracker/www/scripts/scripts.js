7ac77daa1f5d2c88b0d534b6132146c7f95f4837
Hadi Michael <hadisalem7@gmail.com>
2012-07-18 22:13:19 +1000

    more startLearningNow to scripts

--- V-Tracker/www/scripts/scripts.js
+++ V-Tracker/www/scripts/scripts.js
@@ -5,6 +5,28 @@
  *
  */
 
+var newRoute; //TO DO: don't use a global variable
+
+function startLearningNow() {
+	//new route
+	var routeName = $('#newRouteName').val(); //get route name
+	
+	//TO DO: check if route name is free, if not, then prompt! PS. this could happen in validObjName
+	
+	newRoute = new route(routeName); //create a new route object
+	newRoute.alertOps.displayInDiv(true);
+	newRoute.alertOps.divId("#loaderDialog-alertsConsole");
+	newRoute.learn(); //start updating the object
+	
+	//prepare learning dialog
+	$("#loaderDialog-header").html("<h1>Learning...</h1>");
+	$("#loaderDialog-top").html(
+		"<a href=\"javascript:newRoute.endLearn()\" data-role=\"button\" " +
+		"data-theme=\"b\" data-transition=\"none\">Destination reached</a>")
+
+	$.mobile.changePage('#loaderDialog', 'none', true, true);
+}
+
 //************************************* HELPER SCRIPTS ************************************//
 function onPause() {
 	console.log("Device paused!");
