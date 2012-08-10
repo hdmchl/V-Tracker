d457e2dac7f7f90683db694431b925efcd06069d
Hadi Michael <hadisalem7@gmail.com>
2012-08-10 12:47:33 +1000

    fix degrees to radians conversion

--- V-Tracker/www/scripts/vtracker.js
+++ V-Tracker/www/scripts/vtracker.js
@@ -618,10 +618,10 @@ var modellingAPI = {
 	haversineDistance:function(lat1,lon1,lat2,lon2) {
 		//use the Haversine formula to return the distance between two geo points in metres
 		var R = 6371000; // radius of the earth in metres
-		var dLat = (lat2-lat1).toRad();
-		var dLon = (lon2-lon1).toRad();
-		var lat1 = lat1.toRad();
-		var lat2 = lat2.toRad();
+		var dLat = (lat2-lat1) * Math.PI/180;
+		var dLon = (lon2-lon1) * Math.PI/180;
+		var lat1 = lat1 * Math.PI/180;
+		var lat2 = lat2 * Math.PI/180;
 		
 		var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
 		var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
